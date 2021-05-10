3e7a9a935cd8b6507415ce67c9bd747f3cce304f
Nikita Koznev <nikitakoznev@gmail.com>
2021-05-10 20:36:16 +0300

    Added Dockerfile

--- literal/literal/settings.py
+++ literal/literal/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost']
 
 
 # Application definition
