723e946a747586956cd96e29003ff4ee1ca5acb3
Nikita Koznev <nikitakoznev@gmail.com>
2021-05-13 23:23:37 +0300

    Added volumes to nginx

--- literal/literal/urls.py
+++ literal/literal/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-authentication/", include("rest_framework.urls")),
     path("api/v1/authentication/", include("apps.authentication.urls")),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
